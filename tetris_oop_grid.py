ac415744c63875cfe69ca74826cce2a657cd4597
Daniela Bojica <bojicadaniela@gmail.com>
2023-11-28 17:16:40 +0200

    Highlight the lines in grid

--- tetris_oop_grid.py
+++ tetris_oop_grid.py
@@ -1,3 +1,5 @@
+import pygame
+
 
 class Grid():
     def __init__(self):
@@ -5,6 +7,7 @@ class Grid():
         self.num_cols = 10
         self.cell_size = 30
         self.grid = [[0 for j in range(self.num_cols)] for i in range(self.num_rows)]
+        self.color = (102, 102, 153)
 
     def print_grid(self):
         for row in range(self.num_rows):
@@ -12,5 +15,9 @@ class Grid():
                 print(self.grid[row][col], end=' ')
             print()
 
-
-
+    def draw(self, screen):
+        for row in range(self.num_rows):
+            for col in range(self.num_cols):
+                cell_rect = pygame.Rect(col * self.cell_size + 1, row * self.cell_size +1 ,
+                                        self.cell_size - 1, self.cell_size - 1)
+                pygame.draw.rect(screen, self.color, cell_rect)
--- tetris_oop_run.py
+++ tetris_oop_run.py
@@ -3,6 +3,7 @@ import sys
 from tetris_oop_grid import Grid
 
 pygame.init()
+dark_blue = (0, 0, 102)
 
 screen = pygame.display.set_mode((300, 600))
 pygame.display.set_caption("Tetris")
@@ -12,13 +13,14 @@ clock = pygame.time.Clock()
 grid = Grid()
 grid.print_grid()
 
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-    screen.fill('black')
-
+    screen.fill(dark_blue)
+    grid.draw(screen)
     pygame.display.flip()
     clock.tick(60)
