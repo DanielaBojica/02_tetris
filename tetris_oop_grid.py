3dea71f38cdc3cca95c96e7e22b0dce13d0eb1a6
Daniela Bojica <bojicadaniela@gmail.com>
2023-12-02 17:42:37 +0200

    Added list of colors,indexes connected with values for cells

--- tetris_oop_grid.py
+++ tetris_oop_grid.py
@@ -1,13 +1,13 @@
 import pygame
 
 
-class Grid():
+class Grid:
     def __init__(self):
         self.num_rows = 20
         self.num_cols = 10
         self.cell_size = 30
         self.grid = [[0 for j in range(self.num_cols)] for i in range(self.num_rows)]
-        self.color = (102, 102, 153)
+        self.colors = self.get_cell_colors()
 
     def print_grid(self):
         for row in range(self.num_rows):
@@ -15,9 +15,26 @@ class Grid():
                 print(self.grid[row][col], end=' ')
             print()
 
+    # we need a list of colors (8, the first for the empty cells, the remaining 7 for the shapes)
+    # the order matters. The index will be used in connection with each shape id, and to the cell_value (see draw()).
+    # the cell_value = 0 when empty, and varies from 1 to 7 when occupied as part of a shape
+    @staticmethod
+    def get_cell_colors():
+        dark_grey = (26, 31, 40)
+        green = (47, 230, 23)
+        red = (232, 18, 18)
+        orange = (226, 116, 17)
+        yellow = (237, 234, 4)
+        purple = (166, 0, 247)
+        cyan = (21, 204, 209)
+        blue = (13, 64, 216)
+
+        return [dark_grey, green, red, orange, yellow, purple, cyan, blue]
+
     def draw(self, screen):
         for row in range(self.num_rows):
             for col in range(self.num_cols):
-                cell_rect = pygame.Rect(col * self.cell_size + 1, row * self.cell_size +1 ,
+                cell_value = self.grid[row][col]
+                cell_rect = pygame.Rect(col * self.cell_size + 1, row * self.cell_size +1,
                                         self.cell_size - 1, self.cell_size - 1)
-                pygame.draw.rect(screen, self.color, cell_rect)
+                pygame.draw.rect(screen, self.colors[cell_value], cell_rect)
--- tetris_oop_run.py
+++ tetris_oop_run.py
@@ -3,15 +3,21 @@ import sys
 from tetris_oop_grid import Grid
 
 pygame.init()
-dark_blue = (0, 0, 102)
+dark_blue = (44, 44, 127)
 
 screen = pygame.display.set_mode((300, 600))
 pygame.display.set_caption("Tetris")
 
 clock = pygame.time.Clock()
 
-grid = Grid()
-grid.print_grid()
+game_grid = Grid()
+
+game_grid.grid[0][0] = 1
+game_grid.grid[3][5] = 4
+game_grid.grid[17][8] = 7
+game_grid.grid[19][0] = 2
+
+game_grid.print_grid()
 
 
 while True:
@@ -21,6 +27,6 @@ while True:
             sys.exit()
 
     screen.fill(dark_blue)
-    grid.draw(screen)
+    game_grid.draw(screen)
     pygame.display.flip()
     clock.tick(60)
