21550cb0bf9ae8b10208a4e407f269a34ce0158d
Daniela Bojica <bojicadaniela@gmail.com>
2023-12-02 18:37:55 +0200

    Run the game after creating all shapes; shape on screen

--- tetris_oop_run.py
+++ tetris_oop_run.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from tetris_oop_grid import Grid
+from tetris_oop_shapes import *
 
 pygame.init()
 dark_blue = (44, 44, 127)
@@ -12,13 +13,7 @@ clock = pygame.time.Clock()
 
 game_grid = Grid()
 
-game_grid.grid[0][0] = 1
-game_grid.grid[3][5] = 4
-game_grid.grid[17][8] = 7
-game_grid.grid[19][0] = 2
-
-game_grid.print_grid()
-
+shape = LShape()
 
 while True:
     for event in pygame.event.get():
@@ -28,5 +23,7 @@ while True:
 
     screen.fill(dark_blue)
     game_grid.draw(screen)
+    shape.draw(screen)
+
     pygame.display.flip()
     clock.tick(60)
