612a0ec08e9a7e6c858edba90ba245af4013fd34
Daniela Bojica <bojicadaniela@gmail.com>
2023-12-04 15:56:17 +0200

    Correct list of shapes by adding (), and create draw() method

--- tetris_oop_game.py
+++ tetris_oop_game.py
@@ -5,16 +5,22 @@ from tetris_oop_grid import Grid
 from tetris_oop_shapes import *
 import random
 
+
 class Game:
     def __init__(self):
         self.grid = Grid()
-        self.shapes = [IShape, JShape, LShape, OShape, SShape, ZShape,TShape]
+        self.shapes = [IShape(), JShape(), LShape(), OShape(), SShape(), ZShape(), TShape()]
+        self.current_shape = self.get_random_shape()
+        self.next_shape = self.get_random_shape()
 
     # we create a method to get random shapes
     def get_random_shape(self):
         if len(self.shapes) == 0:
-            self.shapes = [IShape, JShape, LShape, OShape, SShape, ZShape, TShape]
+            self.shapes = [IShape(), JShape(), LShape(), OShape(), SShape(), ZShape(), TShape()]
         shape = random.choice(self.shapes)
         self.shapes.remove(shape)
         return shape
 
+    def draw(self, screen):
+        self.grid.draw(screen)
+        self.current_shape.draw(screen)
