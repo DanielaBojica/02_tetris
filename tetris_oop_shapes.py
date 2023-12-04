0e8cf06e94efae4beff39d1ed64b28e3137a01b5
Daniela Bojica <bojicadaniela@gmail.com>
2023-12-04 14:00:12 +0200

    Add 2 attributes and 2 methods to Shape class to show the move of shapes

--- tetris_oop_shapes.py
+++ tetris_oop_shapes.py
@@ -7,12 +7,29 @@ class Shape:
     def __init__(self, id):
         self.id = id
         self.cell_size = 30
+        self.row_offset = 0
+        self.col_offset = 0
         self.cells_rotations = {}  # a dictionary keys=rotation position 0-3, values=list of coordinates of each rect
         self.rotation_state = 0  # which is included in self.cells_rotations as the first key
         self.colors = Colors.get_cell_colors()  # a list whose indexes will be self.id's
 
+    # method to move the shape by adding rows and columns to the cells
+    def move(self, rows, cols):
+        self.row_offset += rows
+        self.col_offset += cols
+
+    # we need to calculate the positions of each cell after the offset is applied
+    # better a new method than in the draw() method, as we need it later to check for collisions
+    def get_cell_positions(self):
+        tiles = self.cells_rotations[self.rotation_state]  # the current rotation state of the shape
+        moved_tiles = []
+        for position in tiles:
+            position = Position(position.row + self.row_offset, position.col + self.col_offset)
+            moved_tiles.append(position)
+        return moved_tiles
+
     def draw(self, screen):
-        tiles = self.cells_rotations[self.rotation_state]
+        tiles = self.get_cell_positions()
         for tile in tiles:
             tile_rect = pygame.Rect(tile.col * self.cell_size + 1, tile.row * self.cell_size + 1, self.cell_size - 1,
                                     self.cell_size - 1)
