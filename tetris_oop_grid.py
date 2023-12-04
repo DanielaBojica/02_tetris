e6e0e6cd180dcabeea98fc5d658bc3479b98bc80
Daniela Bojica <bojicadaniela@gmail.com>
2023-12-04 13:19:37 +0200

    Cancel methods get_cell_colors reallocated and print_grid unnecessary

--- tetris_oop_grid.py
+++ tetris_oop_grid.py
@@ -1,4 +1,5 @@
 import pygame
+from tetris_nick_colors import Colors
 
 
 class Grid:
@@ -7,29 +8,8 @@ class Grid:
         self.num_cols = 10
         self.cell_size = 30
         self.grid = [[0 for j in range(self.num_cols)] for i in range(self.num_rows)]
-        self.colors = self.get_cell_colors()
+        self.colors = Colors.get_cell_colors()
 
-    def print_grid(self):
-        for row in range(self.num_rows):
-            for col in range(self.num_cols):
-                print(self.grid[row][col], end=' ')
-            print()
-
-    # we need a list of colors (8, the first for the empty cells, the remaining 7 for the shapes)
-    # the order matters. The index will be used in connection with each shape id, and to the cell_value (see draw()).
-    # the cell_value = 0 when empty, and varies from 1 to 7 when occupied as part of a shape
-    @staticmethod
-    def get_cell_colors():
-        dark_grey = (26, 31, 40)
-        green = (47, 230, 23)
-        red = (232, 18, 18)
-        orange = (226, 116, 17)
-        yellow = (237, 234, 4)
-        purple = (166, 0, 247)
-        cyan = (21, 204, 209)
-        blue = (13, 64, 216)
-
-        return [dark_grey, green, red, orange, yellow, purple, cyan, blue]
 
     def draw(self, screen):
         for row in range(self.num_rows):
